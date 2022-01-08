fcf3661d5109652840b69531c5b0a11e8b6d4850
wangpatrick57 <wang.patrick57@gmail.com>
2022-01-08 11:36:58 -0800

    generated ALL the data

--- scripts/gen_combined_validation_combined_node_finding_commands.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for species1 in horse human mouse pig rabbit rat; do
-    for species2 in horse human mouse pig rabbit rat; do
-        echo "run_combined_node_finding.sh $species1 $species2"
-    done
-done
