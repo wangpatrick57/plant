e1abec1421c17ac8b1cae0d9f181bff8242bfb17
wangpatrick57 <wang.patrick57@gmail.com>
2022-01-15 18:21:06 -0800

    wrote no bad orbits script

--- scripts/paper_no_bad_orbits.py
+++ scripts/paper_no_bad_orbits.py
@@ -1,6 +1,9 @@
 #!/bin/python3
 from seeding_algorithm_core import *
 from blant_cache_helpers import *
+from odv_helpers import *
+from node_pair_extraction_helpers import *
+from ortholog_helpers import *
 
 # input parameters (none, since there's only one test right now)
 
@@ -10,19 +13,25 @@ species1 = "mouse"
 species2 = "rat"
 lDEG = 2
 percent = 0
-max_length = 15
-sim_threshold = 0.79
-
-# output stored here
+max_indices = 15
+sims_threshold = 0.79
 
 # run for all orbits
 for orbit in range(0, 15):
     # run seeding
     s1_index_path = get_index_path(species1, percent=percent, orbit=orbit, lDEG=lDEG)
     s2_index_path = get_index_path(species2, percent=percent, orbit=orbit, lDEG=lDEG)
+    s1_odv_path = get_odv_file_path(species1)
+    s2_odv_path = get_odv_file_path(species2)
+    seeding_settings = SeedingAlgorithmSettings(max_indices=max_indices, sims_threshold=sims_threshold)
+    all_seeds = find_seeds(k, species1, species2, s1_index_path, s2_index_path, s1_odv_path, s2_odv_path, seeding_settings)
 
-    # extract nodes
+    # extract pairs
+    all_pairs = extract_node_pairs(all_seeds)
 
-    # calculate orthonodes
+    # calculate orthopairs
+    s1_to_s2_orthologs = get_s1_to_s2_orthologs(species1, species2)
+    orthopairs = get_orthopairs_list(all_pairs, s1_to_s2_orthologs)
 
-# print output
+    # print output immediately
+    print(f'{len(orthopairs)}\t{len(all_pairs)}')
--- scripts/run_blant_t_o.sh
+++ scripts/run_blant_t_o.sh
@@ -4,7 +4,7 @@ top_thousandth=$2
 orbit=$3
 el_file="/home/sana/Jurisica/IID/networks/IID${species}.el"
 orca4_file="${el_file}.orca4"
-out_file="${HOME}/plant/data/combine/p${top_thousandth}-o${orbit}-${species}-lDEG2.out"
+out_file="${HOME}/plant/data/seeding_cached_data/blant_out/p${top_thousandth}-o${orbit}-${species}-lDEG2.out"
 
 cd ~/BLANT
 ~/BLANT/blant -k8 -lDEG2 -mi -sINDEX -o$orbit -T$top_thousandth -f$orca4_file $el_file > $out_file
