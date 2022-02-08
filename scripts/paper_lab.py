ee60d28131c5d9653ebcfcc6b2f1cd91e45c8921
wangpatrick57 <wang.patrick57@gmail.com>
2022-02-07 20:06:14 -0800

    more cleanup and notopedge data

--- scripts/paper_lab.py
+++ scripts/paper_lab.py
@@ -3,13 +3,15 @@ from seeding_algorithm_core import *
 from ortholog_helpers import *
 
 species1 = 'mouse'
-species2 = 'human'
+species2 = 'rat'
 
 def get_path(species):
-    return f'/home/wangph1/plant/data/seeding_cached_data/special_blant_out/p0-o0-hayes{species}-lDEG2.out'
-    # return f'/home/wangph1/plant/data/seeding_cached_data/blant_out/p0-o0-{species}-lDEG2.out'
+    # return f'/home/wangph1/plant/data/seeding_cached_data/special_blant_out/p0-o0-hayes{species}-lDEG2.out'
+    return f'/home/wangph1/plant/data/seeding_cached_data/blant_out/p0-o0-{species}-lDEG2.out'
 
+print('before')
 allseeds = find_seeds(8, species1, species2, get_path(species1), get_path(species2), settings=SeedingAlgorithmSettings(max_indices=50, sims_threshold=0.8), print_progress=True)
+print('after')
 orthoseeds = get_orthoseeds_list(allseeds, get_s1_to_s2_orthologs(species1, species2))
 
 print(f'{len(orthoseeds)} / {len(allseeds)}')
