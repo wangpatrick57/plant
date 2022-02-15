9c7baf1050eb859d96b73a16e6498728b9ff8f5c
wangpatrick57 <wang.patrick57@gmail.com>
2022-02-14 19:34:39 -0800

    cleaned up output_helpers

--- scripts/paper_lab.py
+++ scripts/paper_lab.py
@@ -1,17 +1,8 @@
 #!/bin/python3
-from seeding_algorithm_core import *
-from ortholog_helpers import *
+from full_algorithm_helpers import *
 
 species1 = 'mouse'
 species2 = 'rat'
-
-def get_path(species):
-    # return f'/home/wangph1/plant/data/seeding_cached_data/special_blant_out/p0-o0-hayes{species}-lDEG2.out'
-    return f'/home/wangph1/plant/data/seeding_cached_data/blant_out/p0-o0-{species}-lDEG2.out'
-
-print('before')
-allseeds = find_seeds(8, species1, species2, get_path(species1), get_path(species2), settings=SeedingAlgorithmSettings(max_indices=50, sims_threshold=0.8), print_progress=True)
-print('after')
-orthoseeds = get_orthoseeds_list(allseeds, get_s1_to_s2_orthologs(species1, species2))
-
-print(f'{len(orthoseeds)} / {len(allseeds)}')
+orbits = [0]
+sims_threshold = 0.79
+max_indices = 15
