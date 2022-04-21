68b29515a42da89731e66ed9d668408bae9b7975
wangpatrick57 <wang.patrick57@gmail.com>
2022-04-21 16:44:07 -0700

    added temporal graph density mapper

--- scripts/paper_lab.py
+++ scripts/paper_lab.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 from graph_helpers import *
+from temporal_graph_helpers import *
 
-nodes = read_in_nodes(get_graph_path('syeast0'))
-print(len(nodes))
+tel = read_in_temporal_el(get_snap_graph_path('sx-stackoverflow.tel'))
+map_density_over_time(tel)
