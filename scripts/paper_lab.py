b4ee53a48bcff738217cb26f66f28f89cee8b776
wangpatrick57 <wang.patrick57@gmail.com>
2022-04-23 15:15:54 -0700

    created initial sxso temporal networks + code to do that

--- scripts/paper_lab.py
+++ scripts/paper_lab.py
@@ -1,6 +1,15 @@
 #!/bin/python3
 from graph_helpers import *
 from temporal_graph_helpers import *
+from file_helpers import *
 
 graph_path = get_snap_graph_path('sx-stackoverflow.tel')
-get_edge_limit_node_edge_ratios(graph_path, 1217567877, 5000000, 10, 300000)
+start_time = 1_217_567_877
+interval = 15_000_000
+delta = 5_000
+
+while delta < interval:
+    el = read_in_el_in_interval(graph_path, start_time + delta, start_time + delta + interval)
+    file_path = f'/home/wangph1/plant/networks/temporal_slices/sxso_start+{delta}_inter{15_000_000}.el'
+    write_el_to_file(el, file_path)
+    delta *= 4
