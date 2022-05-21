fdb86348cac639212fd9106b1d8818756c7df1ed
wangpatrick57 <wang.patrick57@gmail.com>
2022-05-21 12:43:16 -0700

    added basic run cmd

--- scripts/1plab.py
+++ scripts/1plab.py
@@ -1,9 +1,5 @@
 #!/pkg/python/3.7.4/bin/python3
-import sys
-from index_helpers import *
+from bash_helpers import *
 
-index1_path = get_index_path('syeast0', 0, 0, lDEG=2, alph=True)
-index2_path = get_index_path('syeast0', 0, 0, lDEG=2, alph=False)
-index1 = read_in_index(index1_path, 8)
-index2 = read_in_index(index2_path, 8)
-print(f'{index1_path} == {index2_path}?', index1 == index2)
+out, err = run_cmd('echo hello')
+print(out, err)
