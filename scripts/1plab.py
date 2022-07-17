9a9bd2f0f6919f6f75c16862c162e7620b961614
wangpatrick57 <wang.patrick57@gmail.com>
2022-07-17 16:07:08 -0700

    added all of data to gitignore

--- scripts/1plab.py
+++ scripts/1plab.py
@@ -3,4 +3,4 @@ from all_helpers import *
 import sys
 
 for gtag in get_paper_all_gtags(base_tprl_only=True):
-    print(gtag, gtag_to_real_name(gtag))
+    print(gtag, gtag_to_real_name(gtag), graph_stats(gtag))
--- scripts/full_algorithm_helpers.py
+++ scripts/full_algorithm_helpers.py
@@ -115,7 +115,7 @@ def results_with_alphrev(gtag1, gtag2):
 
 # one step down from the money maker
 # low param means T=0, M=1, p=0, o=0 with two index and graph files
-def low_param_one_run(s1_index_path, s1_graph_path, s2_index_path, s2_graph_path, s1_to_s2_orthologs, prox=1, target_num_matching=1):
+def low_param_one_run(s1_index_path, s1_graph_path, s2_index_path, s2_graph_path, s1_to_s2_orthologs, prox=2, target_num_matching=1):
     k = 8
     s1_index = get_patched_index(k, s1_index_path, s1_graph_path, prox=prox, target_num_matching=target_num_matching)
     s2_index = get_patched_index(k, s2_index_path, s2_graph_path, prox=prox, target_num_matching=target_num_matching)
@@ -124,14 +124,14 @@ def low_param_one_run(s1_index_path, s1_graph_path, s2_index_path, s2_graph_path
     seeds = find_seeds(s1_index, s2_index, ODVDirectory(get_odv_path('syeast0', 5)), ODVDirectory(get_odv_path('syeast0', 5)), SeedingAlgorithmSettings(max_indices=1, sims_threshold=0), print_progress=False)
     return seeds
 
-def get_all_metrics(seeds, g1_to_g2_ort):
+def get_all_metrics(seeds, g1_to_g2_ort, gtag1='', gtag2=''):
     from analysis_helpers import get_avg_size, get_seed_nc
 
     avg_size = get_avg_size(seeds)
     seed_nc = get_seed_nc(seeds, g1_to_g2_ort)
     seed_metrics = (avg_size, seed_nc)
 
-    all_node_pairs = extract_node_pairs(seeds)
+    all_node_pairs = extract_node_pairs(seeds, ignore_deg_1=True, gtag1=gtag1, gtag2=gtag2)
     extr_vol = len(all_node_pairs)
     extr_nc = len(get_orthopairs_list(all_node_pairs, g1_to_g2_ort))
     extr_metrics = (extr_vol, extr_nc)
@@ -156,7 +156,7 @@ def money_maker(gtag1, gtag2, algo='bno', overwrite=False, silent=False):
         seeds = read_in_seeds(seeds_path)
 
     g1_to_g2_ort = get_g1_to_g2_orthologs(gtag1, gtag2)
-    seed_metrics, extr_metrics = get_all_metrics(seeds, g1_to_g2_ort)
+    seed_metrics, extr_metrics = get_all_metrics(seeds, g1_to_g2_ort, gtag1=gtag1, gtag2=gtag2)
     return (seeds, seed_metrics, extr_metrics)
 
 def seed_to_str(seed):
