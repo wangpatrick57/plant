0b406993c38b01c298ff5f91d528c5389df44e37
Brian S <brians3476@gmail.com>
2022-07-22 19:47:48 -0700

    Added rest of functions to relevant files (analysis_helpers, node_pair_extraction_helpers)

--- scripts/node_pair_extraction_helpers.py
+++ scripts/node_pair_extraction_helpers.py
@@ -3,6 +3,62 @@ from collections import defaultdict
 from file_helpers import *
 from ortholog_helpers import *
 
+def add_valid_edges(list_of_species_nodes, lines):
+    num_edges = 0
+    edges = set()
+    for i in list_of_species_nodes:
+        if(i[0] in lines and i[1] in lines):
+            num_edges += 1
+            edges.add((i[0], i[1]))
+    return [edges, num_edges]
+
+def bifurcate_mapping_into_dictionaries(preprocessed_nodes, left_node_frequencies, right_node_frequencies):
+    counter = 0
+    for i in preprocessed_nodes[0]:
+        if i != "0" and preprocessed_nodes[1][counter] != "0":
+            left_node_frequencies[i] += 1
+            right_node_frequencies[preprocessed_nodes[1][counter]] += 1
+            counter += 1
+    return [left_node_frequencies, right_node_frequencies]
+
+def remove_duplicate_mappings_from_dictionary(preprocessed_nodes, left_node_frequencies, right_node_frequencies):
+    processed_list_of_nodes = list()
+    counter = 0
+    for i in preprocessed_nodes[0]:
+        if i != "0" and preprocessed_nodes[1][counter] != "0":
+            if not(left_node_frequencies[i] >= 1 or int(right_node_frequencies[preprocessed_nodes[1][counter]]) >= 1):
+                processed_list_of_nodes.append((i, preprocessed_nodes[1][counter]))
+        counter += 1
+    return processed_list_of_nodes
+
+def remove_dupes(list_of_nodes):
+
+    left_node_frequencies = defaultdict(int) # Using Python's defaultdict
+    right_node_frequencies = defaultdict(int) # Using Python's defaultdict
+    final_list = list()
+
+    left_node_frequencies = bifurcate_mapping_into_dictionaries(list_of_nodes, left_node_frequencies, right_node_frequencies)[0]
+    right_node_frequencies = bifurcate_mapping_into_dictionaries(list_of_nodes, left_node_frequencies, right_node_frequencies)[1]
+    
+    final_list = remove_duplicate_mappings_from_dictionary(list_of_nodes, left_node_frequencies, right_node_frequencies)
+
+    return final_list
+
+def filtering_lists(name):
+
+    processed_list = list()
+
+    temporary_list = list_of_nodes_from_species(name)
+
+    reformatted_nodes = []
+    for i in temporary_list:
+        reformatted_nodes.append(map(str, i))
+    result = list(map(list, zip(*reformatted_nodes)))
+
+    processed_list = remove_dupes(result)
+
+    return processed_list
+
 def extract_node_pairs(all_seeds_list):
     m2m_pairs = seeds_to_m2m(all_seeds_list)
     return extract_node_pairs_from_m2m(m2m_pairs)
