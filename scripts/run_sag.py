7c23f2dec2adba1867b2b21e46daa6c8ae97e529
wangpatrick57 <wang.patrick57@gmail.com>
2023-03-02 08:59:46 -0800

    modified run sag to allow k_max as well

--- scripts/run_sag.py
+++ scripts/run_sag.py
@@ -2,23 +2,32 @@
 import sys
 from all_helpers import *
 
-def get_sag_alignment_path(gtag1, gtag2, auto_k=None):
-    return get_data_path(f'simanneal/{gtag1}-{gtag2}-w{auto_k[0]}-p{int(auto_k[1] * 100)}.align')
+def get_sag_alignment_path(gtag1, gtag2, k_max=None, auto_k=None):
+    assert not (k_max == None and auto_k == None)
+    assert not (k_max != None and auto_k != None)
+    
+    if auto_k != None:
+        return get_data_path(f'simanneal/{gtag1}-{gtag2}-w{auto_k[0]}-p{int(auto_k[1] * 100)}.align')
+    elif k_max != None:
+        return get_data_path(f'simanneal/{gtag1}-{gtag2}-k{k_max}.align')
+    else:
+        raise AssertionError()
 
 if __name__ == '__main__':
     from seeding_algorithm_core import SeedingAlgorithmSettings
     
     gtag1 = sys.argv[1]
     gtag2 = sys.argv[2]
-    overwrite = True
+    k_max = int(sys.argv[3])
+    # auto_k = (10000, 0.01)
+    auto_k = None
 
     algo = 'stairs'
-    # auto_k = (10000, 0.01)
-    auto_k = (100, 0.01)
+    overwrite = True
     adj_set1 = read_in_adj_set(get_graph_path(gtag1))
     adj_set2 = read_in_adj_set(get_graph_path(gtag2))
     g1_to_g2_ort = get_g1_to_g2_orthologs(gtag1, gtag2)
-    sag_alignment_path = get_sag_alignment_path(gtag1, gtag2, auto_k=auto_k)
+    sag_alignment_path = get_sag_alignment_path(gtag1, gtag2, k_max=k_max, auto_k=auto_k)
     time_path = '.'.join(sag_alignment_path.split('.')[:-1]) + '.time'
     pair = f'{gtag1}-{gtag2}'
 
@@ -29,7 +38,7 @@ if __name__ == '__main__':
         seeds, _, _ = simplified_run_with_metrics(gtag1, gtag2)
         blocks = seeds_to_blocks(seeds)
         sagrow = SimAnnealGrow(blocks, adj_set1, adj_set2, p_func=ALWAYS_P_FUNC, s3_threshold=1)
-        alignment = sagrow.run(auto_k=auto_k, silent=False)
+        alignment = sagrow.run(k_max=k_max, auto_k=auto_k, silent=False)
         alignment = get_clean_alignment(alignment, adj_set1, adj_set2)
         end_time = time.time()
         write_to_file(alignment_to_str(alignment), sag_alignment_path)
