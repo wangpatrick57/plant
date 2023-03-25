a75b199d34e6f18200776c2cc4b9b1920d7eebb3
wangpatrick57 <wang.patrick57@gmail.com>
2023-03-25 13:33:43 -0700

    running tprl

--- scripts/seeds_slurm_tool.py
+++ scripts/seeds_slurm_tool.py
@@ -9,12 +9,13 @@ from all_helpers import *
 # sometimes the log files contain partial output before the program is actually done. don't be surprised by this
 
 # pairs = get_iid_mammal_pairs()
+pairs = get_tprl_pairs()
 # pairs = get_iid_representative_pairs()
-pairs = get_tprl_representative_pairs()
+# pairs = get_tprl_representative_pairs()
 MAX_INDICES = 1
-sims_threshold_list = [-0.8, -0.82, -0.84, -0.86, -0.88, -0.9, -0.92, -0.94, -0.96, -0.98]
+# sims_threshold_list = [-0.8, -0.82, -0.84, -0.86, -0.88, -0.9, -0.92, -0.94, -0.96, -0.98]
+sims_threshold_list = [-0.95]
 
 for gtag1, gtag2 in pairs:
     for sims_threshold in sims_threshold_list:
         print(f'./run_seeds.py {gtag1} {gtag2} {MAX_INDICES} {sims_threshold}')
-
