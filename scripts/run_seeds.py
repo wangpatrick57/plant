a4efc4ced68deac2d2ba4c32c6b43fb6e1622e02
wangpatrick57 <wang.patrick57@gmail.com>
2023-03-26 14:09:47 -0700

    added args for run seeds

--- scripts/run_seeds.py
+++ scripts/run_seeds.py
@@ -1,13 +1,16 @@
 #!/bin/python3
 import sys
 from all_helpers import *
+import argparse
 
 if __name__ == '__main__':
     from seeding_algorithm_core import SeedingAlgorithmSettings
-    
-    gtag1 = sys.argv[1]
-    gtag2 = sys.argv[2]
-    max_indices = int(sys.argv[3])
-    sims_threshold = float(sys.argv[4])
-    
-    seeds, _, _ = simplified_run_with_metrics(gtag1, gtag2, settings=SeedingAlgorithmSettings(max_indices=max_indices, sims_threshold=sims_threshold))
+
+    parser = argparse.ArgumentParser(prog='run_seeds.py', description='Run the seed finding algorithm (Algorithm 2 in the paper)')
+    parser.add_argument('gtag1')
+    parser.add_argument('gtag2')
+    parser.add_argument('-m', '--max-indices', default=1, help='The maximum number of times the graphlet may appear in either index')
+    parser.add_argument('-s', '--sims-threshold', default=-0.95, help='The minimum (if s>0) or maximum (if s<0) mean ODV similarity value of the graphlet pair')
+    args = parser.parse_args()
+    print(args)
+    seeds, _, _ = simplified_run_with_metrics(args.gtag1, args.gtag2, settings=SeedingAlgorithmSettings(max_indices=args.max_indices, sims_threshold=args.sims_threshold))
