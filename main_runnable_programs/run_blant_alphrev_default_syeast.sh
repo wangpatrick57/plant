be99d66037686f200168a0b776b82c32ad832fea
wangpatrick57 <wang.patrick57@gmail.com>
2023-03-27 11:28:21 -0700

    removed unnecessary run_blant*sh files

--- main_runnable_programs/run_blant_alphrev_default_syeast.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yeast_name=$1
-el_file="${HOME}/BLANT/networks/${yeast_name}/${yeast_name}.el"
-alph_file="${HOME}/plant/data/seeding_cached_data/blant_out/p0-o0-${yeast_name}-lDEG2-alph.out"
-rev_file="${HOME}/plant/data/seeding_cached_data/blant_out/p0-o0-${yeast_name}-lDEG2-rev.out"
-
-run_blant_default_custom.sh $el_file 2 1 $alph_file
-run_blant_default_custom.sh $el_file 2 0 $rev_file
